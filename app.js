05c627f3a51e142fd6cc1820a70df130f1051614
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-24 12:05:15 +0300

    :construction: add crud operations

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
-  fs.readFileSync('./dev-data/data/tours.json', 'utf-8')
+  fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8')
 );
 
 app.get('/api/v1/tours', (req, res) => {
@@ -19,7 +19,72 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: {
       tours
-    },
+    }
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const tourId = req.params.id * 1;
+  const tour = tours.find(({ id }) => id === tourId);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tour,
+    }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours), (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data:null
   })
 })
 
