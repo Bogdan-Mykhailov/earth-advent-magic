3cdb2afaa32ebe6d8665b173adfc7b3d58019fb1
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-24 12:57:42 +0300

    :construction: refactor code

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 'use strict';
-import fs from 'fs';
 import express from 'express';
 import dotenv from 'dotenv';
+import {
+  TOURS_URL,
+  addTour,
+  deleteTour,
+  getAllTours,
+  getOneTour,
+  updateTour
+} from './helpers.js';
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -9,84 +16,16 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-const tours = JSON.parse(
-  fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8')
-);
+app
+  .route(`${TOURS_URL.baseUrl}`)
+  .get(getAllTours)
+  .post(addTour);
 
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
-});
-
-app.get('/api/v1/tours/:id', (req, res) => {
-  const tourId = req.params.id * 1;
-  const tour = tours.find(({ id }) => id === tourId);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours: tour,
-    }
-  });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours), (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
-});
-
-app.patch('/api/v1/tours/:id', (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>'
-    }
-  })
-})
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID'
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data:null
-  })
-})
+app
+  .route(`${TOURS_URL.baseUrl}/:id`)
+  .get(getOneTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(PORT, () => {
   console.log(`Server use port ${PORT}`);
