2b2c053f93dcb614ddd97449537794482bc08ee1
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-25 14:46:04 +0300

    :construction: connect to db

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { app } from './app.js';
 dotenv.config({ path: './config.env' });
 
-import { app } from './app.js';
+const DB = process.env.DATABASE
+  .replace('<USERNAME>', process.env.DB_USERNAME)
+  .replace('<PASSWORD>', process.env.DB_PASSWORD);
 
+mongoose
+  .connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => console.log('DB connection successful!'));
 
 const PORT = process.env.PORT || 8000;
 
