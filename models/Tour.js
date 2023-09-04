8d4342312179f8b7a952a9fe327965f8f6e4175e
Bogdan_Mykhailov <bogdan_mykhailov@icloud.com>
2023-09-04 15:14:48 +0300

    :construction: add document, query, aggregation middlewares

--- models/Tour.js
+++ models/Tour.js
@@ -1,5 +1,6 @@
 'use strict';
 import mongoose from 'mongoose';
+import slugify from 'slugify';
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour must have a duration']
@@ -36,24 +38,64 @@ const tourSchema = new mongoose.Schema({
   priceDiscount: Number,
   summary: {
     type: String,
-    trim: true,
+    required: [true, 'A tour must have a summary'],
+    trim: true
   },
   description: {
     type: String,
-    trim: true,
-    required: [true, 'A tour must have a description']
+    trim: true
   },
   imageCover: {
     type: String,
     required: [true, 'A tour must have a cover image']
   },
-  images:[String],
+  images: [String],
+  secretTour: {
+    type: Boolean,
+    default: false
+  },
   createdAt: {
     type: Date,
     default: Date.now(),
     select: false
   },
   startDates: [Date]
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
+
+tourSchema.virtual('durationWeeks').get(function() {
+  // use fnExpression coz need 'this' keyword
+  return this.duration / 7;
+});
+
+// document middleware: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// query middleware
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  next();
+})
+
+// aggregation middleware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } }
+  })
+  console.log(this);
+  next();
+})
 
 export const Tour = mongoose.model('Tour', tourSchema);
