48205e6374e1a30bd8b9738e665e6a680754a7be
Bogdan_Mykhailov <bogdan_mykhailov@icloud.com>
2023-09-05 22:15:32 +0300

    :construction: handling invalid db fields

--- controllers/Error.js
+++ controllers/Error.js
@@ -1,12 +1,24 @@
 'use strict';
-import { STATUSES } from '../utils/constants.js';
+import { sendErrorDev, sendErrorProd, STATUSES } from '../utils/constants.js';
+import { AppError } from '../utils/error.js';
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+
+  return new AppError(message, 400);
+};
 
 export const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || STATUSES.ERROR;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-}
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
+};
--- utils/constants.js
+++ utils/constants.js
@@ -16,3 +16,34 @@ export const STATUSES = {
   SUCCESS: 'success',
   ERROR: 'error',
 }
+
+export const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+export const sendErrorProd = (err, res) => {
+  // operational, trusted error: send message to client
+
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // programing or other unknown error: don't leak error details
+  } else {
+    // 1. log error
+    console.log('error', err);
+
+    // 2. send generic message
+    res.status(500).json({
+      status: STATUSES.ERROR,
+      message: 'Something went wrong!'
+    });
+  }
+};
