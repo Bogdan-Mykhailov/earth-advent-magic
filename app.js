7e0ea70369c327f741c082c3eec35afa8af6d942
Bogdan_Mykhailov <bogdan_mykhailov@icloud.com>
2023-09-12 10:43:06 +0300

    :construction: implement secure features setting security http headers, data sanitization, prevent parameter pollution

--- app.js
+++ app.js
@@ -7,24 +7,57 @@ import { userRouter } from './routes/User.js';
 import { AppError } from './utils/error.js';
 import { globalErrorHandler } from './controllers/Error.js';
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import hpp from 'hpp';
 
 export const app = express();
 
 // 1 global middlewares
+// set security http headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit requests from same api
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'To many requests from this IP, please try again in an hour!'
 });
-app.use(`${TOURS_URL.api}`, limiter)
+app.use(`${TOURS_URL.api}`, limiter);
 
-app.use(express.json());
+// body parser, reading data from the body into req.body
+app.use(express.json({
+  limit: '10kb'
+}));
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against XSS
+app.use(xss());
+
+// prevent parameter pollution
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'ratingsQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price'
+  ]
+}));
+
+// serving static files
 app.use(express.static(PUBLIC_PATH));
 
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
