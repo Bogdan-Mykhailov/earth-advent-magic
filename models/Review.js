9bd6ece876cc00d2148e25427f820331ebfcf4ad
Bogdan_Mykhailov <bogdan_mykhailov@icloud.com>
2023-09-15 11:19:32 +0300

    :construction: prevent duplicate reviews

--- models/Review.js
+++ models/Review.js
@@ -1,6 +1,7 @@
 'use strict';
 import mongoose from 'mongoose';
 import { ROLES } from '../utils/constants.js';
+import { Tour } from './Tour.js';
 
 const reviewSchema = new mongoose.Schema({
   review: {
@@ -18,25 +19,64 @@ const reviewSchema = new mongoose.Schema({
     select: false
   },
   tour: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'Tour',
-      required: [true, 'Review must belong to a tour.']
-    },
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Review must belong to a tour.']
+  },
   user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: [true, 'Review must belong to a user.']
-    }
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to a user.']
+  }
 }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
 });
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next) {
   this.populate({
     path: `${ROLES.user}`,
-    select: 'name photo'});
+    select: 'name photo'
+  });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats.length > 0 ? stats[0].nRating : 0,
+    ratingsAverage: stats.length > 0 ? stats[0].avgRating : 4.5
+  });
+};
+
+reviewSchema.post('save', function() {
+  // this point to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^fingOneAnd/, async function(next) {
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function(docs) {
+  if (docs) {
+    await docs.constructor.calcAverageRatings(docs.tour);
+  }
 });
 
 export const Review = mongoose.model('Review', reviewSchema);
